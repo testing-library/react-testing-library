cea3f5ba4b5bb773affa04868ef1ffdce6740aeb
Daniel Sandiego <dnlsandiego@gmail.com>
2018-03-19 21:49:01 -0500

    chore: rename internal utility sel to select (#5)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import ReactDOM from 'react-dom'
 import {Simulate} from 'react-dom/test-utils'
 
 // we may expose this eventually
-function sel(id) {
+function select(id) {
   return `[data-testid="${id}"]`
 }
 
 // we may expose this eventually
 function queryDivByTestId(div, id) {
-  return div.querySelector(sel(id))
+  return div.querySelector(select(id))
 }
 
 function render(ui, {container = document.createElement('div')} = {}) {
