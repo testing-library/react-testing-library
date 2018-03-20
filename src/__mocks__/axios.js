88db98404f83189178615de18830cc22cf69bf77
Alejandro Ñáñez Ortiz <alejonanez@gmail.com>
2018-03-20 10:01:57 -0500

    Update axios.js
    Adding docs about the other HTTP methods.

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,3 +1,8 @@
 module.exports = {
   get: jest.fn(() => Promise.resolve({data: {}})),
 }
+
+// Note:
+// For now we don't need any other method (POST/PUT/PATCH), what we have already works fine.
+// We will add more methods only if we need to.
+// For reference please read: https://github.com/kentcdodds/react-testing-library/issues/2
