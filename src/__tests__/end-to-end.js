cef06a88b8ffd0e01b03b62434b6f9df3941e738
Łukasz Gandecki <lgandecki@css.edu>
2018-03-28 17:31:59 +0400

    added waitForExpect with test

---
+++ src/__tests__/end-to-end.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {render, waitForExpect} from '../'
+
+const fetchAMessage = () =>
+  new Promise(resolve => {
+    // we are using random timeout here to simulate a real-time example
+    // of an async operation calling a callback at a non-deterministic time
+    const randomTimeout = Math.floor(Math.random() * 100)
+    setTimeout(() => {
+      resolve({returnedMessage: 'Hello World'})
+    }, randomTimeout)
+  })
+
+class ComponentWithLoader extends React.Component {
+  state = {loading: true}
+  async componentDidMount() {
+    const data = await fetchAMessage()
+    this.setState({data, loading: false}) // eslint-disable-line
+  }
+  render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div data-testid="message">
+          Loaded this message: {this.state.data.returnedMessage}!
+        </div>
+      )
+    }
+  }
+}
+
+test('it waits for the data to be loaded', async () => {
+  const {queryByText, queryByTestId} = render(<ComponentWithLoader />)
+
+  expect(queryByText('Loading...')).toBeTruthy()
+
+  await waitForExpect(() => {
+    expect(queryByText('Loading...')).toBeNull()
+    expect(queryByTestId('message').textContent).toMatch(/Hello World/)
+  })
+})
