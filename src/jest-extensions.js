a225906e92b621d784c1c504edf100eedcbbd39b
Ernesto Garcia <gnapse@gmail.com>
2018-04-05 20:43:23 -0300

    Remove unneeded if/else logic in custom matchers

--- src/jest-extensions.js
+++ src/jest-extensions.js
@@ -44,28 +44,15 @@ function getAttributeComment(name, value) {
 const extensions = {
   toBeInTheDOM(received) {
     getDisplayName(received)
-    if (received) {
-      return {
-        message: () =>
-          `${matcherHint(
-            '.not.toBeInTheDOM',
-            'received',
-            '',
-          )} Expected the element not to be present` +
-          `\nReceived : ${printReceived(received)}`,
-        pass: true,
-      }
-    } else {
-      return {
-        message: () =>
-          `${matcherHint(
-            '.toBeInTheDOM',
-            'received',
-            '',
-          )} Expected the element to be present` +
-          `\nReceived : ${printReceived(received)}`,
-        pass: false,
-      }
+    return {
+      message: () =>
+        `${matcherHint(
+          `${this.isNot ? '.not' : ''}.toBeInTheDOM`,
+          'received',
+          '',
+        )} Expected the element not to be present` +
+        `\nReceived : ${printReceived(received)}`,
+      pass: !!received,
     }
   },
 
@@ -73,28 +60,15 @@ const extensions = {
     checkHtmlElement(htmlElement)
     const textContent = htmlElement.textContent
     const pass = matches(textContent, htmlElement, checkWith)
-    if (pass) {
-      return {
-        message: () =>
-          assertMessage(
-            '.not.toHaveTextContent',
-            'Expected value not equals to',
-            htmlElement,
-            checkWith,
-          ),
-        pass: true,
-      }
-    } else {
-      return {
-        message: () =>
-          assertMessage(
-            '.toHaveTextContent',
-            'Expected value equals to',
-            htmlElement,
-            checkWith,
-          ),
-        pass: false,
-      }
+    return {
+      message: () =>
+        assertMessage(
+          `${this.isNot ? '.not' : ''}.toHaveTextContent`,
+          `Expected value ${this.isNot ? 'not ' : ''}equals to`,
+          htmlElement,
+          checkWith,
+        ),
+      pass,
     }
   },
 
