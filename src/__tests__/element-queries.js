c1910084bb96604f27cb133136cf1b893fe5f945
Ernesto Garcia <gnapse@gmail.com>
2018-04-05 18:02:48 -0300

    Add tests to cover toHaveAttribute custom matcher

--- src/__tests__/element-queries.js
+++ src/__tests__/element-queries.js
@@ -117,4 +117,34 @@ test('using jest helpers to assert element states', () => {
   ).toThrowError()
 })
 
+test('using jest helpers to check element attributes', () => {
+  const {queryByTestId} = render(
+    <button data-testid="ok-button" type="submit" disabled>
+      OK
+    </button>,
+  )
+
+  expect(queryByTestId('ok-button')).toHaveAttribute('disabled')
+  expect(queryByTestId('ok-button')).toHaveAttribute('type')
+  expect(queryByTestId('ok-button')).not.toHaveAttribute('class')
+  expect(queryByTestId('ok-button')).toHaveAttribute('type', 'submit')
+  expect(queryByTestId('ok-button')).not.toHaveAttribute('type', 'button')
+
+  expect(() =>
+    expect(queryByTestId('ok-button')).not.toHaveAttribute('disabled'),
+  ).toThrowError()
+  expect(() =>
+    expect(queryByTestId('ok-button')).not.toHaveAttribute('type'),
+  ).toThrowError()
+  expect(() =>
+    expect(queryByTestId('ok-button')).toHaveAttribute('class'),
+  ).toThrowError()
+  expect(() =>
+    expect(queryByTestId('ok-button')).not.toHaveAttribute('type', 'submit'),
+  ).toThrowError()
+  expect(() =>
+    expect(queryByTestId('ok-button')).toHaveAttribute('type', 'button'),
+  ).toThrowError()
+})
+
 /* eslint jsx-a11y/label-has-for:0 */
