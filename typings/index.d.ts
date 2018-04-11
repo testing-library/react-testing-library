e38ce02a6a3d4363350c12f9fbec59f2a4c06fbe
Thomas Chia <thomas.h.chia@googlemail.com>
2018-04-11 23:39:39 +0800

    fix(TypeScript): add a definition for TextMatch (#52)
    * Add a definition for TextMatch and replace the query helper arguments with it.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,18 +1,21 @@
 import {Simulate as ReactSimulate} from 'react-dom/test-utils'
 
+type TextMatchFunction = (content: string, element: HTMLElement) => boolean
+type TextMatch = string | RegExp | TextMatchFunction
+
 interface RenderResult {
   container: HTMLDivElement
   unmount: VoidFunction
-  queryByTestId: (id: string) => HTMLElement | null
-  getByTestId: (id: string) => HTMLElement
-  queryByText: (id: string) => HTMLElement | null
-  getByText: (id: string) => HTMLElement
-  queryByPlaceholderText: (id: string) => HTMLElement | null
-  getByPlaceholderText: (id: string) => HTMLElement
-  queryByLabelText: (id: string) => HTMLElement | null
-  getByLabelText: (id: string) => HTMLElement
-  queryByAltText: (text: string) => HTMLElement | null
-  getByAltText: (text: string) => HTMLElement
+  queryByTestId: (id: TextMatch) => HTMLElement | null
+  getByTestId: (id: TextMatch) => HTMLElement
+  queryByText: (id: TextMatch) => HTMLElement | null
+  getByText: (text: TextMatch) => HTMLElement
+  queryByPlaceholderText: (id: TextMatch) => HTMLElement | null
+  getByPlaceholderText: (text: TextMatch) => HTMLElement
+  queryByLabelText: (text: TextMatch) => HTMLElement | null
+  getByLabelText: (id: TextMatch, options?: {selector: string}) => HTMLElement
+  queryByAltText: (text: TextMatch) => HTMLElement | null
+  getByAltText: (text: TextMatch) => HTMLElement
 }
 
 export function render(
