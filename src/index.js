40dfcde4fa29e04912b895a76599f8b09937bc1a
Kent C. Dodds <kent@doddsfamily.us>
2018-04-24 14:04:57 -0600

    feat(queries): upgrade dom-testing-library to support all queries

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import ReactDOM from 'react-dom'
 import {Simulate} from 'react-dom/test-utils'
-import {queries, wait, fireEvent} from 'dom-testing-library'
+import {bindElementToQueries, wait, fireEvent} from 'dom-testing-library'
 
 function render(ui, {container = document.createElement('div')} = {}) {
   ReactDOM.render(ui, container)
-  const containerHelpers = Object.entries(queries).reduce(
-    (helpers, [key, fn]) => {
-      helpers[key] = fn.bind(null, container)
-      return helpers
-    },
-    {},
-  )
   return {
     container,
     unmount: () => ReactDOM.unmountComponentAtNode(container),
-    ...containerHelpers,
+    ...bindElementToQueries(container),
   }
 }
 
