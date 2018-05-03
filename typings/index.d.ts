0aae0e15443948cd1acb7cf9a4acb621492b3661
Maddi Joyce <maddi@maddijoyce.com>
2018-05-04 09:37:59 +1000

    fix(TS): add types for exported functions. (#69)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -33,3 +33,94 @@ export function wait(
     interval?: number
   },
 ): Promise<void>
+
+export function waitForElement<T>(
+  callback?: () => T,
+  options?: {
+    container?: HTMLElement
+    timeout?: number
+    mutationObserverOptions?: MutationObserverInit
+  },
+): Promise<T | undefined>
+
+type EventType =
+  | 'copy'
+  | 'cut'
+  | 'paste'
+  | 'compositionEnd'
+  | 'compositionStart'
+  | 'compositionUpdate'
+  | 'keyDown'
+  | 'keyPress'
+  | 'keyUp'
+  | 'focus'
+  | 'blur'
+  | 'change'
+  | 'input'
+  | 'invalid'
+  | 'submit'
+  | 'click'
+  | 'contextMenu'
+  | 'dblClick'
+  | 'drag'
+  | 'dragEnd'
+  | 'dragEnter'
+  | 'dragExit'
+  | 'dragLeave'
+  | 'dragOver'
+  | 'dragStart'
+  | 'drop'
+  | 'mouseDown'
+  | 'mouseEnter'
+  | 'mouseLeave'
+  | 'mouseMove'
+  | 'mouseOut'
+  | 'mouseOver'
+  | 'mouseUp'
+  | 'select'
+  | 'touchCancel'
+  | 'touchEnd'
+  | 'touchMove'
+  | 'touchStart'
+  | 'scroll'
+  | 'wheel'
+  | 'abort'
+  | 'canPlay'
+  | 'canPlayThrough'
+  | 'durationChange'
+  | 'emptied'
+  | 'encrypted'
+  | 'ended'
+  | 'loadedData'
+  | 'loadedMetadata'
+  | 'loadStart'
+  | 'pause'
+  | 'play'
+  | 'playing'
+  | 'progress'
+  | 'rateChange'
+  | 'seeked'
+  | 'seeking'
+  | 'stalled'
+  | 'suspend'
+  | 'timeUpdate'
+  | 'volumeChange'
+  | 'waiting'
+  | 'load'
+  | 'error'
+  | 'animationStart'
+  | 'animationEnd'
+  | 'animationIteration'
+  | 'transitionEnd'
+  | 'doubleClick'
+
+type FireFunction = (element: HTMLElement, event: Event) => boolean
+type FireObject = {
+  [K in EventType]: (element: HTMLElement, options?: {}) => boolean
+}
+
+export const fireEvent: FireFunction & FireObject
+
+export function renderIntoDocument(ui: React.ReactElement<any>): RenderResult
+
+export function cleanup(): void
