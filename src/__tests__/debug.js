03cb1b69f8c0562a276b5c24a9237df8acd9522a
Kent C. Dodds <kent@doddsfamily.us>
2018-05-15 09:24:58 -0600

    feat(debug): add a debug convenience method
    Also adds @Gpx to contributors!
    
    Closes #86

---
+++ src/__tests__/debug.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {render} from '../'
+
+beforeEach(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => {})
+})
+
+afterEach(() => {
+  console.log.mockRestore()
+})
+
+test('debug pretty prints the container', () => {
+  const HelloWorld = () => <h1>Hello World</h1>
+  const {debug} = render(<HelloWorld />)
+  debug()
+  expect(console.log).toHaveBeenCalledTimes(1)
+  expect(console.log).toHaveBeenCalledWith(
+    expect.stringContaining('Hello World'),
+  )
+})
+
+/* eslint no-console:0 */
