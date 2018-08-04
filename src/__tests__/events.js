8d9c2fa2903554d1f5120bfd9e4ae0979a2e233e
Kent C. Dodds <kent@doddsfamily.us>
2018-08-04 07:45:42 -0600

    WIP: add setNativeValue for change event

--- src/__tests__/events.js
+++ src/__tests__/events.js
@@ -156,10 +156,12 @@ eventTypes.forEach(({type, events, elementType, init}) => {
 })
 
 test('onChange works', () => {
-  const handleChange = jest.fn()
-  const {
-    container: {firstChild: input},
-  } = render(<input onChange={handleChange} />)
-  fireEvent.change(input, {target: {value: 'a'}})
-  expect(handleChange).toHaveBeenCalledTimes(1)
+  const spy = jest.fn()
+
+  const {container} = render(<input onChange={spy} />)
+  const input = container.firstChild
+  fireEvent.change(input, {target: {value: 'c'}})
+  expect(spy).toHaveBeenCalledTimes(1)
 })
+
+/* eslint complexity:0 */
