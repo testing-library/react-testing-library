08731f281c75477ad5b22f7cef6d0d8c134bc762
chrbala <chrbala@gmail.com>
2018-12-15 17:47:50 -0800

    feat(hydrate): add ability to test component hydration (#246)

--- src/__tests__/rerender.js
+++ src/__tests__/rerender.js
@@ -11,3 +11,25 @@ test('rerender will re-render the element', () => {
   rerender(<Greeting message="hey" />)
   expect(container.firstChild).toHaveTextContent('hey')
 })
+
+test('hydrate will not update props until next render', () => {
+  const initial = '<input />'
+
+  const container = document.body.appendChild(document.createElement('div'))
+  container.innerHTML = initial
+  const input = container.querySelector('input')
+  const firstValue = 'hello'
+
+  if (!input) throw new Error('No element')
+  input.value = firstValue
+  const {rerender} = render(<input value="" onChange={() => null} />, {
+    container,
+    hydrate: true,
+  })
+
+  const secondValue = 'goodbye'
+
+  expect(input.value).toBe(firstValue)
+  rerender(<input value={secondValue} onChange={() => null} />)
+  expect(input.value).toBe(secondValue)
+})
