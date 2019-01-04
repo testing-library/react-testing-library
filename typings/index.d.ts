730ee2a5cff28be862fe0203207e7d4bc811d554
Cho Chi Him <chochihim@gmail.com>
2019-01-05 00:23:26 +0800

    fix(TS): add queries typings (#259)
    <!--
    Thanks for your interest in the project. Bugs filed and PRs submitted are appreciated!
    
    Please make sure that you are familiar with and follow the Code of Conduct for
    this project (found in the CODE_OF_CONDUCT.md file).
    
    Also, please make sure you're familiar with and follow the instructions in the
    contributing guidelines (found in the CONTRIBUTING.md file).
    
    If you're new to contributing to open source projects, you might find this free
    video course helpful: http://kcd.im/pull-request
    
    Please fill out the information below to expedite the review and (hopefully)
    merge of your pull request!
    -->
    
    <!-- What changes are being made? (What feature/bug is being fixed here?) -->
    
    **What**: Add typings for queries in RenderOptions
    
    <!-- Why are these changes necessary? -->
    
    **Why**: It's missing. Also in a project I need to add some custom queries.
    
    <!-- How were these changes implemented? -->
    
    **How**:
    
    <!-- Have you done all of these things?  -->
    
    **Checklist**:
    
    <!-- add "N/A" to the end of each line that's irrelevant to your changes -->
    
    <!-- to check an item, place an "x" in the box like so: "- [x] Documentation" -->
    
    - [ ] Documentation
    - [ ] Tests
    - [ ] Ready to be merged
          <!-- In your opinion, is this ready to be merged as soon as it's reviewed? -->
    - [ ] Added myself to contributors table
          <!-- this is optional, see the contributing guidelines for instructions -->
    
    <!-- feel free to add additional comments -->
    The implementation is trickier than I thought. Any comment is appreciated. Also `Query` and `Queries` should probably be declared in `dom-testing-library`. I can create another PR once the implementation of this PR is accepted.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,31 +1,44 @@
-import {getQueriesForElement} from 'dom-testing-library'
+import {queries, BoundFunction} from 'dom-testing-library'
 
 export * from 'dom-testing-library'
 
-type GetsAndQueries = ReturnType<typeof getQueriesForElement>
+interface Query extends Function {
+  (container: HTMLElement, ...args): HTMLElement[] | HTMLElement | null
+}
 
-export interface RenderResult extends GetsAndQueries {
+interface Queries {
+  [T: string]: Query
+}
+
+export type RenderResult<Q extends Queries = typeof queries> = {
   container: HTMLElement
   baseElement: HTMLElement
   debug: (baseElement?: HTMLElement | DocumentFragment) => void
   rerender: (ui: React.ReactElement<any>) => void
   unmount: () => boolean
   asFragment: () => DocumentFragment
-}
+} & {[P in keyof Q]: BoundFunction<Q[P]>}
 
-export interface RenderOptions {
+export interface RenderOptions<Q extends Queries = typeof queries> {
   container?: HTMLElement
   baseElement?: HTMLElement
   hydrate?: boolean
+  queries?: Q
 }
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 /**
  * Render into a container which is appended to document.body. It should be used with cleanup.
  */
 export function render(
   ui: React.ReactElement<any>,
-  options?: RenderOptions,
+  options?: Omit<RenderOptions, 'queries'>,
 ): RenderResult
+export function render<Q extends Queries>(
+  ui: React.ReactElement<any>,
+  options: RenderOptions<Q>,
+): RenderResult<Q>
 
 /**
  * Unmounts React trees that were mounted with render.
