83e2ef57356519f8358c3bdb65d39adb285caef8
donavon <github@donavon.com>
2019-01-22 14:25:56 -0500

    add another test

--- src/__tests__/testHook.js
+++ src/__tests__/testHook.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import 'jest-dom/extend-expect'
 import {testHook, cleanup} from '../'
 
@@ -7,4 +8,7 @@ test('testHook calls the callback', () => {
   const spy = jest.fn()
   testHook(spy)
   expect(spy).toHaveBeenCalledTimes(1)
+})
+test('confirm we can safely call a React Hook from within the callback', () => {
+  testHook(() => useState())
 })
