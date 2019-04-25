d232c0063d80fe0cf5e79bf00bc53c228981a3f0
Kent C. Dodds <kent@doddsfamily.us>
2019-04-25 15:14:27 -0600

    feat: update to the latest dom-testing-library@4.0.0 (#365)
    BREAKING CHANGE: This upgrades to the latest dom-testing-library which has a few breaking changes: https://github.com/testing-library/dom-testing-library/releases/tag/v4.0.0

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -5,4 +5,4 @@ module.exports = {
 // Note:
 // For now we don't need any other method (POST/PUT/PATCH), what we have already works fine.
 // We will add more methods only if we need to.
-// For reference please read: https://github.com/kentcdodds/react-testing-library/issues/2
+// For reference please read: https://github.com/testing-library/react-testing-library/issues/2
