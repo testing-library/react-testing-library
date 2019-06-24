8e99f3cf99b76afbf2b20993745b802def3bb6c9
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-06-24 20:46:02 +0200

    fix: Specify baseElement without container (#394)
    * Add expected behavior of base element
    
    * Fix expected behavior of baseElement
    
    * Improve tests

--- src/__tests__/render.js
+++ src/__tests__/render.js
@@ -3,8 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {render, cleanup} from '../'
 
-afterEach(cleanup)
-
 test('renders div into document', () => {
   const ref = React.createRef()
   const {container} = render(<div ref={ref} />)
