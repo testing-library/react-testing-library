605a8b16f4b5a8bb3fb5457119877807bba6306a
Kent C. Dodds <kent@doddsfamily.us>
2019-08-08 15:50:51 -0600

    feat(cleanup): automatically cleanup if afterEach is detected
    You can disable this with the RTL_SKIP_CLEANUP environment variable if
    you so choose, but it's recommended to have cleanup work this way.
    
    Closes #428

---
+++ src/__tests__/auto-cleanup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render} from '../'
+
+// This just verifies that by importing RTL in an
+// environment which supports afterEach (like jest)
+// we'll get automatic cleanup between tests.
+test('first', () => {
+  render(<div>hi</div>)
+})
+
+test('second', () => {
+  expect(document.body.innerHTML).toEqual('')
+})
