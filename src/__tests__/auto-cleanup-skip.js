e2d25724dc5b0f9b29655a75c8a58ae24780a307
Kent C. Dodds <kent@doddsfamily.us>
2019-08-08 15:50:51 -0600

    feat(cleanup): automatically cleanup if afterEach is detected
    You can disable this with the RTL_SKIP_CLEANUP environment variable if
    you so choose, but it's recommended to have cleanup work this way.
    
    Closes #428
    
    BREAKING CHANGE: If your tests were not isolated before (and you referenced the same component between tests) then this change will break your tests. You should [keep your tests isolated](https://kentcdodds.com/blog/test-isolation-with-react), but if you're unable/unwilling to do that right away, then you can either run your tests with the environment variable `RTL_SKIP_AUTO_CLEANUP` set to `true` or import `@testing-library/react/pure` instead of `@testing-library/react`.

--- src/__tests__/auto-cleanup-skip.js
+++ src/__tests__/auto-cleanup-skip.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 let render
 beforeAll(() => {
-  process.env.RTL_SKIP_CLEANUP = 'true'
+  process.env.RTL_SKIP_AUTO_CLEANUP = 'true'
   const rtl = require('../')
   render = rtl.render
 })
 
-// This one verifies that if RTL_SKIP_CLEANUP is set
+// This one verifies that if RTL_SKIP_AUTO_CLEANUP is set
 // then we DON'T auto-wire up the afterEach for folks
 test('first', () => {
   render(<div>hi</div>)
