139df628291511a9ce0c3a559c2c245f7ce64747
Ronald van der Kooij <ronald@sharpsolutions.nl>
2019-09-03 23:39:31 +0200

    fix: console.error doesn't have to be a string in act.compat.js (#476)
    * checking the type of args in act-compat before doing an indexOf
    
    * removed unused code
    
    * noop scenario for console.error was not tested in act-compat

--- src/__tests__/old-act.js
+++ src/__tests__/old-act.js
@@ -98,4 +98,45 @@ test('async act recovers from sync errors', async () => {
   `)
 })
 
+test('async act can handle any sort of console.error', async () => {
+  await asyncAct(async () => {
+    console.error({error: 'some error'})
+    await null
+  })
+
+  expect(console.error).toHaveBeenCalledTimes(2)
+  expect(console.error.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        Object {
+          "error": "some error",
+        },
+      ],
+      Array [
+        "It looks like you're using a version of react-dom that supports the \\"act\\" function, but not an awaitable version of \\"act\\" which you will need. Please upgrade to at least react-dom@16.9.0 to remove this warning.",
+      ],
+    ]
+  `)
+})
+
+test('async act should not show an error when ReactTestUtils.act returns something', async () => {
+  jest.resetModules()
+  jest.mock('react-dom/test-utils', () => ({
+    act: () => {
+      return new Promise(resolve => {
+        console.error(
+          'Warning: The callback passed to ReactTestUtils.act(...) function must not return anything',
+        )
+        resolve()
+      })
+    },
+  }))
+  asyncAct = require('../act-compat').asyncAct
+  await asyncAct(async () => {
+    await null
+  })
+
+  expect(console.error).toHaveBeenCalledTimes(0)
+})
+
 /* eslint no-console:0 */
