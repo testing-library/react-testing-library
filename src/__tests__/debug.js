f2805d47bf2a9d5490dcbc0dd5b934d67d1e48a4
Daniel Afonso <35337607+danieljcafonso@users.noreply.github.com>
2019-10-03 20:16:53 +0100

    feat(debug): allow debugging an array of containers (#495)

--- src/__tests__/debug.js
+++ src/__tests__/debug.js
@@ -19,4 +19,21 @@ test('debug pretty prints the container', () => {
   )
 })
 
+test('debug pretty prints multiple containers', () => {
+  const HelloWorld = () => (
+    <>
+      <h1 data-testid="testId">Hello World</h1>
+      <h1 data-testid="testId">Hello World</h1>
+    </>
+  )
+  const {getAllByTestId, debug} = render(<HelloWorld />)
+  const multipleElements = getAllByTestId('testId')
+  debug(multipleElements)
+
+  expect(console.log).toHaveBeenCalledTimes(2)
+  expect(console.log).toHaveBeenCalledWith(
+    expect.stringContaining('Hello World'),
+  )
+})
+
 /* eslint no-console:0 */
