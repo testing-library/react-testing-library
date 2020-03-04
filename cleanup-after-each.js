9bd87c68b74c072a9f7c717a4d99482b03bdfe92
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-04 12:52:19 -0700

    fix: remove deprecated cleanup-after-each (#598)
    BREAKING CHANGE: This removes `@testing-library/react/cleanup-after-each` which is deprecated and hasn't done anything for many versions.

--- cleanup-after-each.js
+++
@@ -1,4 +0,0 @@
-console.warn(
-  'The module `@testing-library/react/cleanup-after-each` has been deprecated and no longer does anything (it is not needed). You no longer need to import this module and can safely remove any import or configuration which imports this module',
-)
-/* eslint no-console:0 */
