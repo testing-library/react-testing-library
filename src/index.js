4626ba120a1c43a84607169a469995d9fdc402a0
Christian Rackerseder <git@echooff.de>
2020-05-27 08:04:43 +0200

    Run cleanup() when test runner supports teardown()

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import {cleanup} from './pure'
 
 // if we're running in a test runner that supports afterEach
-// then we'll automatically run cleanup afterEach test
+// or teardown then we'll automatically run cleanup afterEach test
 // this ensures that tests run in isolation from each other
 // if you don't like this then either import the `pure` module
 // or set the RTL_SKIP_AUTO_CLEANUP env variable to 'true'.
-if (typeof afterEach === 'function' && !process.env.RTL_SKIP_AUTO_CLEANUP) {
-  afterEach(async () => {
-    await cleanup()
-  })
+if (!process.env.RTL_SKIP_AUTO_CLEANUP) {
+  if (typeof afterEach === 'function') {
+    afterEach(async () => {
+      await cleanup()
+    })
+  } else if (typeof teardown === 'function') {
+    teardown(async () => {
+      await cleanup()
+    })
+  }
 }
 
 export * from './pure'
