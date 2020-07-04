5ef6cb8a813b505b0cd9c8018cfabe8b57b1ccfb
Matan Borenkraout <matanbobi@gmail.com>
2020-07-04 23:43:26 +0300

    Working on ^16.9 and lower than 16.8

--- src/flush-microtasks.js
+++ src/flush-microtasks.js
@@ -1,7 +1,3 @@
-import {
-  unstable_scheduleCallback as scheduleCallback,
-  unstable_NormalPriority as normalPriority,
-} from 'scheduler'
 /* istanbul ignore file */
 // the part of this file that we need tested is definitely being run
 // and the part that is not cannot easily have useful tests written
@@ -21,6 +17,10 @@ function getIsUsingFakeTimers() {
 
 let didWarnAboutMessageChannel = false
 let enqueueTask
+const globalObj = typeof window === 'undefined' ? global : window
+
+let Scheduler = globalObj.Scheduler
+
 try {
   // read require off the module object to get around the bundlers.
   // we don't want them to detect a require and bundle a Node polyfill.
@@ -29,6 +29,8 @@ try {
   // assuming we're in node, let's try to get node's
   // version of setImmediate, bypassing fake timers if any.
   enqueueTask = nodeRequire.call(module, 'timers').setImmediate
+  // import React's scheduler so we'll be able to schedule our tasks later on.
+  Scheduler = nodeRequire.call(module, 'scheduler')
 } catch (_err) {
   // we're in a browser
   // we can't use regular timers because they may still be faked
@@ -53,6 +55,15 @@ try {
   }
 }
 
+// in case this react version has a Scheduler implementation, we use it,
+// if not, we just create a function calling our callback
+const scheduleCallback = Scheduler
+  ? Scheduler.scheduleCallback || Scheduler.unstable_scheduleCallback
+  : (_, cb) => cb()
+const NormalPriority = Scheduler
+  ? Scheduler.NormalPriority || Scheduler.unstable_NormalPriority
+  : null
+
 export default function flushMicroTasks() {
   return {
     then(resolve) {
@@ -63,7 +74,7 @@ export default function flushMicroTasks() {
         jest.advanceTimersByTime(0)
         resolve()
       } else {
-        scheduleCallback(normalPriority, () => {
+        scheduleCallback(NormalPriority, () => {
           enqueueTask(() => {
             resolve()
           })
