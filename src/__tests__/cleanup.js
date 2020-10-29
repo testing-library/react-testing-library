de4352bf598c8326de505a0480a29d4276c8db65
Michaël De Boey <info@michaeldeboey.be>
2020-10-29 20:00:52 +0100

    chore: update React imports

--- src/__tests__/cleanup.js
+++ src/__tests__/cleanup.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import {render, cleanup} from '../'
 
 test('cleans up the document', () => {
--- src/__tests__/end-to-end.js
+++ src/__tests__/end-to-end.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import {render, waitForElementToBeRemoved, screen} from '../'
 
 const fetchAMessage = () =>
