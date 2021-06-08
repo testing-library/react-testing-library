a06995d8993f543fe8a72c25fbacd97361f140df
eps1lon <silbermann.sebastian@gmail.com>
2021-06-08 21:20:40 +0200

    Ignore coverage for now

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const {jest: jestConfig} = require('kcd-scripts/config')
+
+module.exports = Object.assign(jestConfig, {
+  coverageThreshold: {
+    ...jestConfig.coverageThreshold,
+    // full coverage across the build matrix (React 17, 18) but not in a single job
+    './src/pure': {
+      // minimum coverage of jobs using React 17 and 18
+      branches: 85,
+      functions: 76,
+      lines: 81,
+      statements: 81,
+    },
+  },
+})
