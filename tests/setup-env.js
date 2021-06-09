cea49efef6e284f54051018988b392febce55267
eps1lon <silbermann.sebastian@gmail.com>
2021-06-09 07:01:59 +0200

    Mark scheduler mocking as TODO

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -19,5 +19,6 @@ afterEach(() => {
   consoleErrorMock.mockRestore()
 })
 
+// TODO: Can be removed in a future React release: https://github.com/reactwg/react-18/discussions/23#discussioncomment-798952
 // eslint-disable-next-line import/no-extraneous-dependencies -- need the version from React not an explicitly declared one
 jest.mock('scheduler', () => require('scheduler/unstable_mock'))
