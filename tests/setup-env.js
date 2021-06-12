487eb851d96406557859524eab3fabae82dd3d18
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2021-06-12 11:05:11 +0200

    test: Ignore React 18 legacy root deprecation warnings (#928)

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,1 +1,21 @@
 import '@testing-library/jest-dom/extend-expect'
+
+beforeEach(() => {
+  const originalConsoleError = console.error
+  jest
+    .spyOn(console, 'error')
+    .mockImplementation((message, ...optionalParams) => {
+      // Ignore ReactDOM.render/ReactDOM.hydrate deprecation warning
+      if (message.indexOf('Use createRoot instead.') !== -1) {
+        return
+      }
+      originalConsoleError(message, ...optionalParams)
+    })
+})
+
+afterEach(() => {
+  // maybe another test already restore console error mocks
+  if (typeof console.error.mockRestore === 'function') {
+    console.error.mockRestore()
+  }
+})
