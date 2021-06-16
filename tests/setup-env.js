2ac2377ee89edef467576d7e4daa96e1146c9982
eps1lon <silbermann.sebastian@gmail.com>
2021-06-16 10:36:07 +0200

    Revert "test: Ignore React 18 legacy root deprecation warnings (#929)"
    This reverts commit c1878a9ea68a3a13982233684b924a917e87a1f6.

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,24 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
 
-let consoleErrorMock
-
-beforeEach(() => {
-  const originalConsoleError = console.error
-  consoleErrorMock = jest
-    .spyOn(console, 'error')
-    .mockImplementation((message, ...optionalParams) => {
-      // Ignore ReactDOM.render/ReactDOM.hydrate deprecation warning
-      if (message.indexOf('Use createRoot instead.') !== -1) {
-        return
-      }
-      originalConsoleError(message, ...optionalParams)
-    })
-})
-
-afterEach(() => {
-  consoleErrorMock.mockRestore()
-})
-
 // TODO: Can be removed in a future React release: https://github.com/reactwg/react-18/discussions/23#discussioncomment-798952
 // eslint-disable-next-line import/no-extraneous-dependencies -- need the version from React not an explicitly declared one
 jest.mock('scheduler', () => require('scheduler/unstable_mock'))
