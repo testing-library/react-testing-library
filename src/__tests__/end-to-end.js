b2f3968d13c640aa1934475374fe1cdb6be75e44
eps1lon <silbermann.sebastian@gmail.com>
2022-10-08 13:54:28 +0200

    Add intended behavior

--- src/__tests__/end-to-end.js
+++ src/__tests__/end-to-end.js
@@ -1,73 +1,150 @@
 import * as React from 'react'
 import {render, waitForElementToBeRemoved, screen, waitFor} from '../'
 
-const fetchAMessage = () =>
-  new Promise(resolve => {
-    // we are using random timeout here to simulate a real-time example
-    // of an async operation calling a callback at a non-deterministic time
-    const randomTimeout = Math.floor(Math.random() * 100)
-    setTimeout(() => {
-      resolve({returnedMessage: 'Hello World'})
-    }, randomTimeout)
-  })
-
-function ComponentWithLoader() {
-  const [state, setState] = React.useState({data: undefined, loading: true})
-  React.useEffect(() => {
-    let cancelled = false
-    fetchAMessage().then(data => {
-      if (!cancelled) {
-        setState({data, loading: false})
-      }
+describe.each([
+  ['real timers', () => jest.useRealTimers()],
+  ['fake legacy timers', () => jest.useFakeTimers('legacy')],
+  ['fake modern timers', () => jest.useFakeTimers('modern')],
+])(
+  'it waits for the data to be loaded in a macrotask using %s',
+  (label, useTimers) => {
+    beforeEach(() => {
+      useTimers()
     })
 
-    return () => {
-      cancelled = true
+    afterEach(() => {
+      jest.useRealTimers()
+    })
+
+    const fetchAMessageInAMacrotask = () =>
+      new Promise(resolve => {
+        // we are using random timeout here to simulate a real-time example
+        // of an async operation calling a callback at a non-deterministic time
+        const randomTimeout = Math.floor(Math.random() * 100)
+        setTimeout(() => {
+          resolve({returnedMessage: 'Hello World'})
+        }, randomTimeout)
+      })
+
+    function ComponentWithMacrotaskLoader() {
+      const [state, setState] = React.useState({data: undefined, loading: true})
+      React.useEffect(() => {
+        let cancelled = false
+        fetchAMessageInAMacrotask().then(data => {
+          if (!cancelled) {
+            setState({data, loading: false})
+          }
+        })
+
+        return () => {
+          cancelled = true
+        }
+      }, [])
+
+      if (state.loading) {
+        return <div>Loading...</div>
+      }
+
+      return (
+        <div data-testid="message">
+          Loaded this message: {state.data.returnedMessage}!
+        </div>
+      )
     }
-  }, [])
 
-  if (state.loading) {
-    return <div>Loading...</div>
-  }
+    test('waitForElementToBeRemoved', async () => {
+      render(<ComponentWithMacrotaskLoader />)
+      const loading = () => screen.getByText('Loading...')
+      await waitForElementToBeRemoved(loading)
+      expect(screen.getByTestId('message')).toHaveTextContent(/Hello World/)
+    })
 
-  return (
-    <div data-testid="message">
-      Loaded this message: {state.data.returnedMessage}!
-    </div>
-  )
-}
+    test('waitFor', async () => {
+      render(<ComponentWithMacrotaskLoader />)
+      // eslint-disable-next-line testing-library/prefer-find-by -- Sir, this is a test.
+      await waitFor(() => screen.getByText(/Loading../))
+      // eslint-disable-next-line testing-library/prefer-find-by -- Sir, this is a test.
+      await waitFor(() => screen.getByText(/Loaded this message:/))
+      expect(screen.getByTestId('message')).toHaveTextContent(/Hello World/)
+    })
+
+    test('findBy', async () => {
+      render(<ComponentWithMacrotaskLoader />)
+      await expect(screen.findByTestId('message')).resolves.toHaveTextContent(
+        /Hello World/,
+      )
+    })
+  },
+)
 
 describe.each([
   ['real timers', () => jest.useRealTimers()],
   ['fake legacy timers', () => jest.useFakeTimers('legacy')],
   ['fake modern timers', () => jest.useFakeTimers('modern')],
-])('it waits for the data to be loaded using %s', (label, useTimers) => {
-  beforeEach(() => {
-    useTimers()
-  })
+])(
+  'it waits for the data to be loaded in a microtask using %s',
+  (label, useTimers) => {
+    beforeEach(() => {
+      useTimers()
+    })
 
-  afterEach(() => {
-    jest.useRealTimers()
-  })
+    afterEach(() => {
+      jest.useRealTimers()
+    })
 
-  test('waitForElementToBeRemoved', async () => {
-    render(<ComponentWithLoader />)
-    const loading = () => screen.getByText('Loading...')
-    await waitForElementToBeRemoved(loading)
-    expect(screen.getByTestId('message')).toHaveTextContent(/Hello World/)
-  })
+    const fetchAMessageInAMicrotask = () =>
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({title: 'Hello World'}),
+      })
 
-  test('waitFor', async () => {
-    render(<ComponentWithLoader />)
-    const message = () => screen.getByText(/Loaded this message:/)
-    await waitFor(message)
-    expect(screen.getByTestId('message')).toHaveTextContent(/Hello World/)
-  })
+    function ComponentWithMicrotaskLoader() {
+      const [fetchState, setFetchState] = React.useState({fetching: true})
 
-  test('findBy', async () => {
-    render(<ComponentWithLoader />)
-    await expect(screen.findByTestId('message')).resolves.toHaveTextContent(
-      /Hello World/,
-    )
-  })
-})
+      React.useEffect(() => {
+        if (fetchState.fetching) {
+          fetchAMessageInAMicrotask().then(res => {
+            return res.json().then(data => {
+              setFetchState({todo: data.title, fetching: false})
+            })
+          })
+        }
+      }, [fetchState])
+
+      if (fetchState.fetching) {
+        return <p>Loading..</p>
+      }
+
+      return (
+        <div data-testid="message">Loaded this message: {fetchState.todo}</div>
+      )
+    }
+
+    test('waitForElementToBeRemoved', async () => {
+      render(<ComponentWithMicrotaskLoader />)
+      const loading = () => screen.getByText('Loading..')
+      await waitForElementToBeRemoved(loading)
+      expect(screen.getByTestId('message')).toHaveTextContent(/Hello World/)
+    })
+
+    test('waitFor', async () => {
+      render(<ComponentWithMicrotaskLoader />)
+      await waitFor(() => {
+        // eslint-disable-next-line testing-library/prefer-explicit-assert -- Sir, this is a test.
+        screen.getByText('Loading..')
+      })
+      await waitFor(() => {
+        // eslint-disable-next-line testing-library/prefer-explicit-assert -- Sir, this is a test.
+        screen.getByText(/Loaded this message:/)
+      })
+      expect(screen.getByTestId('message')).toHaveTextContent(/Hello World/)
+    })
+
+    test('findBy', async () => {
+      render(<ComponentWithMicrotaskLoader />)
+      await expect(screen.findByTestId('message')).resolves.toHaveTextContent(
+        /Hello World/,
+      )
+    })
+  },
+)
