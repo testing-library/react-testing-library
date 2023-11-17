fd52a593a7987a14d3cf5c94f112795a1630725d
Matan Borenkraout <Matanbobi@gmail.com>
2023-11-17 16:46:53 +0200

    fix: log globals warning only once (#1252)
    Resolves #1249

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ if (typeof process === 'undefined' || !process.env?.RTL_SKIP_AUTO_CLEANUP) {
     teardown(() => {
       cleanup()
     })
-  } else {
+  } else if (!process.env.RTL_AFTEREACH_WARNING_LOGGED) {
+    process.env.RTL_AFTEREACH_WARNING_LOGGED = true
     console.warn(
       `The current test runner does not support afterEach/teardown hooks. This means we won't be able to run automatic cleanup and you should be calling cleanup() manually.`,
     )
@@ -39,7 +40,8 @@ if (typeof process === 'undefined' || !process.env?.RTL_SKIP_AUTO_CLEANUP) {
     afterAll(() => {
       setReactActEnvironment(previousIsReactActEnvironment)
     })
-  } else {
+  } else if (!process.env.RTL_AFTERALL_WARNING_LOGGED) {
+    process.env.RTL_AFTERALL_WARNING_LOGGED = true
     console.warn(
       'The current test runner does not support beforeAll/afterAll hooks. This means you should be setting IS_REACT_ACT_ENVIRONMENT manually.',
     )
