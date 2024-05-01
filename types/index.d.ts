b6e59f7d58c69f617cc9854d8c3e6f86c2dfb0e9
Matan Borenkraout <Matanbobi@gmail.com>
2024-05-01 21:29:24 +0300

    fix: export new act when available (#1319)

--- types/index.d.ts
+++ types/index.d.ts
@@ -7,7 +7,9 @@ import {
   prettyFormat,
   Config as ConfigDTL,
 } from '@testing-library/dom'
-import {act as reactAct} from 'react-dom/test-utils'
+import {act as reactDeprecatedAct} from 'react-dom/test-utils'
+//@ts-ignore
+import {act as reactAct} from 'react'
 
 export * from '@testing-library/dom'
 
@@ -245,10 +247,10 @@ export function renderHook<
 export function cleanup(): void
 
 /**
- * Simply calls ReactDOMTestUtils.act(cb)
+ * Simply calls React.act(cb)
  * If that's not available (older version of react) then it
- * simply calls the given callback immediately
+ * simply calls the deprecated version which is ReactTestUtils.act(cb)
  */
-export const act: typeof reactAct extends undefined
-  ? (callback: () => void) => void
+export const act: typeof reactAct extends never
+  ? typeof reactDeprecatedAct
   : typeof reactAct
