e7045db8562847e3af5a9c85a7fbdb52f4351160
eps1lon <sebastian.silbermann@vercel.com>
2024-08-28 15:54:19 +0200

    Fix React Canary and Experimental tests

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,5 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
 import './failOnUnexpectedConsoleCalls'
 import {TextEncoder} from 'util'
+import {MessageChannel} from 'worker_threads'
 
 global.TextEncoder = TextEncoder
+// TODO: Revisit once https://github.com/jsdom/jsdom/issues/2448 is resolved
+// This isn't perfect but good enough.
+global.MessageChannel = MessageChannel
